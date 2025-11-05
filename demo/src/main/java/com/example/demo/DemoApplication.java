bfc544e130345a226bcd90ef4f8d853addb506ea
CarlShelby23 <josecarlos23amador@gmail.com>
2025-11-05 13:54:09 -0600

    Actualización

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("Hola web");
+	}
+
+	@RequestMapping("/")
+	public String home() {
+		System.out.println("Bienvenido");
+		return "Bienvenido";
+	}
+
+	@RequestMapping("/hola")
+	public String hola() {
+		System.out.println("Hola");
+		return "Hola";
+	}
+
+	@RequestMapping("/adios")
+	public String adios() {
+		System.out.println("Adiós");
+		return "Adiós";
+	}
+
+	@RequestMapping("/html")
+	public String html() {
+		String html = "<html><body><h1>Hola HTML</h1></body></html>";
+		return html;
+	}
+}
